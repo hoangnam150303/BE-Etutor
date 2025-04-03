8a1787c1b2eb726e6683f86f86fbbdc09b2e9c5a
hoangnam150303 <hoangnam150303@gmail.com>
2025-04-03 17:42:33 +0700

    fix code

--- src/server.js
+++ src/server.js
@@ -44,7 +44,7 @@ app.use("/course", courseRoute);
 app.use("/message", messageRoute);
 app.use("/call", callRoute);
 app.use("/blog", blogRoute);
-// Start server
+
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server is working on port: ${port}`);
   connectDb();
